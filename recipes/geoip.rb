acb8e75891c5bca3056acea1ca4892beda4ac01d
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2012-10-16 12:58:44 +0100

    refactor: move geoIp data resource to the outer scope

--- recipes/geoip.rb
+++ recipes/geoip.rb
@@ -3,6 +3,15 @@ package "GeoIP-devel"
 package "wget"
 
 
+bash "download geolite data" do
+    cwd "/tmp"
+    code <<-EOH
+        rm -f "GeoIP.dat.gz" && wget "http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz" || exit 1
+        zcat "GeoIP.dat.gz" > "/usr/share/GeoIP/GeoIP.dat"
+EOH
+    not_if "test -f /usr/share/GeoIP/GeoIP.dat"
+end
+
 if File.exists?("/etc/varnish/geoip.vcl")
     log("varnish geoip extension is already installed") { level :info }
 else
@@ -10,15 +19,6 @@ else
     git '/usr/src/varnish-geoip' do
         repository 'git://github.com/svalaskevicius/varnish-geoip.git'
         reference 'develop'
-    end
-
-    bash "download geolite data" do
-        cwd "/tmp"
-        code <<-EOH
-            rm -f "GeoIP.dat.gz" && wget "http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz" || exit 1
-            zcat "GeoIP.dat.gz" > "/usr/share/GeoIP/GeoIP.dat"
-EOH
-        not_if "test -f /usr/share/GeoIP/GeoIP.dat"
     end
 
     bash "compile varnish geoip ext" do
