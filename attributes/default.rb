7c1436698f0bb66d33cbb8c4cb3838c862b1447e
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-06-05 16:07:23 +0100

    configure varnish release rpm

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,3 +20,5 @@ default['varnish']['VARNISH_STORAGE_FILE'] = "/var/lib/varnish/varnish_storage.b
 default['varnish']['VARNISH_STORAGE_SIZE'] = "1G"
 default['varnish']['VARNISH_STORAGE'] = "malloc" # file | malloc | persistent
 default['varnish']['VARNISH_TTL'] = 120
+default['varnish']['release_rpm'] = 'http://repo.varnish-cache.org/redhat/varnish-3.0/el5/noarch/varnish-release-3.0-1.noarch.rpm'
+
