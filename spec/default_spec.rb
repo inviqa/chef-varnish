a0a162f2fa1006463bafe2095f38c6e0138b002a
Kieren Evans <kevans@inviqa.com>
2015-01-28 22:42:03 +0000

    Spec for the daemon options order change.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -5,7 +5,7 @@ describe 'chef-varnish::default' do
     let (:chef_run) { ChefSpec::SoloRunner.new(platform: 'centos', version: '6.5').converge(described_recipe) }
 
     it 'should accept an array of launch parameters that are empty by default' do
-      expect(chef_run.node['varnish']['custom_parameters']).to eq([])
+      expect(chef_run.node['varnish']['custom_parameters']).to eq({})
     end
 
     it 'should use the launch parameters in the daemon options' do
@@ -26,7 +26,7 @@ describe 'chef-varnish::default' do
 
     it 'should use the launch parameters in the daemon options' do
       expect(chef_run).to render_file('/etc/sysconfig/varnish')
-        .with_content(match(/-S \${VARNISH_SECRET_FILE} \\\s*-p esi_syntax=0x2 \\\s*-p cli_buffer=16384 \\\s*-s \${VARNISH_STORAGE}/m))
+        .with_content(match(/-s \${VARNISH_STORAGE} \\\s*-p esi_syntax=0x2 \\\s*-p cli_buffer=16384/m))
     end
   end
 end
