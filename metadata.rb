8d445e04740873f69faa4a1f261033453844baba
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2017-09-15 13:22:50 +0100

    Remove unsupported os type hints

--- metadata.rb
+++ metadata.rb
@@ -11,7 +11,7 @@ depends 'yum'
 
 depends 'packagecloud', '~> 0.3.0'
 
-%w(redhat centos scientific fedora debian ubuntu arch freebsd amazon).each do |os|
+%w(redhat centos scientific fedora debian ubuntu amazon).each do |os|
   supports os
 end
 
