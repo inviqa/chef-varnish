b75b32ef89ad339f1476ba2121017a0ef606feaa
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2017-09-15 13:48:33 +0100

    Bump version for minor changes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license          'Apache 2.0'
 description      'Installs/Configures chef-varnish'
 name             'chef-varnish'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.1.0'
 
 depends 'apt'
 depends 'yum'
